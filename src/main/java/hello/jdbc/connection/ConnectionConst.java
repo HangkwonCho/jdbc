97aa265f140b515bb6b4970218671bf07e2e8336
HangkwonCho <vatican0411@gmail.com>
2023-03-21 17:05:31 +0900

    스프링 예외 추상화 적용

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -2,8 +2,8 @@ package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
 
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test_jdbc";
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
     public static final String USERNAME = "sa";
-    public static final String PASSWORD = "1";
+    public static final String PASSWORD = "";
 
 }
